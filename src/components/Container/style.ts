9b48d0aa1773588e629b9fca2dc52eba037fdfca
juniorbonini <juniorboninia7x@gmail.com>
2026-04-10 13:12:32 -0300

    feat: atualiza ícone do aplicativo e ajustes em componentes

--- src/components/Container/style.ts
+++ src/components/Container/style.ts
@@ -1,4 +1,5 @@
 import { Color } from "@/style/Color";
+import { fontFamily } from "@/style/Fontfamily";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,6 +2,7 @@ import { Text, View } from "react-native";
 
 import { TaskSummaryProps } from "@/components/Task/task";
 import { Color } from "@/style/Color";
+import { Gradients } from "@/utils/gradient-linear";
 import { LinearGradient } from "expo-linear-gradient";
 import { styles } from "./style";
 
@@ -10,12 +11,7 @@ export function TaskSummary({ tasksSummary }: TaskSummaryProps) {
     <View style={styles.counter}>
       <View style={styles.created}>
         <Text style={[styles.counterText, styles.created]}>Criadas</Text>
-        <LinearGradient
-          colors={[Color.blue["blue-light"], Color.purple["purple-light"]]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 1 }}
-          style={styles.counterBadge}
-        >
+        <LinearGradient {...Gradients.primary} style={styles.counterBadge}>
           <Text style={styles.value}>{tasksSummary.length}</Text>
         </LinearGradient>
       </View>
